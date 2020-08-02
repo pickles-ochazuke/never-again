c56425df1e93c3b81893019a8a011985dcc45434
Ochadukebiyori <pickles.ochazuke@gmail.com>
2020-08-02 15:56:18 +0900

    コントローラをUI層に配置
    - コントローラの配置位置がずれていたので修正

--- src/main.ts
+++ src/main.ts
@@ -10,16 +10,18 @@ function main(param: g.GameMainParameterObject): void {
     const background = createBackground(scene);
     scene.append(background);
 
-    // コントローラUI
-    const ui = new g.E({scene: scene});
 
     // Player
     const player = createPlayer(scene);
     scene.append(player);
 
-    // 移動 UI
-    const playerController = createPlayerController(scene, player);
-    scene.append(playerController);
+    // コントローラUI
+    const ui = createUi(scene, player);
+    scene.append(ui);
+
+    // // 移動 UI
+    // const playerController = createPlayerController(scene, player);
+    // scene.append(playerController);
   });
 
   g.game.pushScene(scene);
@@ -27,17 +29,42 @@ function main(param: g.GameMainParameterObject): void {
 
 export = main;
 
-function createPlayerController(scene: g.Scene, player: g.E): g.E {
+function createUi(scene: g.Scene, player: g.E): g.E {
 
-  const centerX = g.game.width / 2;
-  const centerY = g.game.height / 2;
+  // UI の描画領域と位置を決める
+  const ui = new g.E({
+    scene: scene,
+    x: 0,
+    y: g.game.height * 0.6,
+    width: g.game.width,
+    height: g.game.height * 0.4
+  });
+
+  // debug用のUI背景
+  ui.append(new g.FilledRect({
+    scene: scene,
+    cssColor: "#8aafebAA",
+    width: ui.width,
+    height: ui.height,
+  }));
+  
+  const controller = createPlayerController(scene, player, ui);
+  ui.append(controller);
+
+  return ui; 
+}
+
+function createPlayerController(scene: g.Scene, player: g.E, parent: g.E): g.E {
+
+  const centerX = parent.width / 2;
+  const centerY = parent.height / 2;
 
   const right = new g.FilledRect({
     scene: scene,
     cssColor: "#FF0000",
     width: 32,
     height: 32,
-    x: centerX + 32,
+    x: centerX + 16,
     y: centerY + 0,
     touchable: true
   });
@@ -60,7 +87,7 @@ function createPlayerController(scene: g.Scene, player: g.E): g.E {
     cssColor: "#00FF00",
     width: 32,
     height: 32,
-    x: centerX - 32,
+    x: centerX - (16 + 32),
     y: centerY + 0,
     touchable: true
   });
@@ -83,7 +110,7 @@ function createPlayerController(scene: g.Scene, player: g.E): g.E {
     cssColor: "#0000FF",
     width: 32,
     height: 32,
-    x: centerX + 0,
+    x: centerX - 16,
     y: centerY + 32,
     touchable: true
   });
@@ -106,7 +133,7 @@ function createPlayerController(scene: g.Scene, player: g.E): g.E {
     cssColor: "#FFFF00",
     width: 32,
     height: 32,
-    x: centerX + 0,
+    x: centerX - 16,
     y: centerY - 32,
     touchable: true
   });
@@ -135,12 +162,26 @@ function createPlayerController(scene: g.Scene, player: g.E): g.E {
 
 function createBackground(scene: g.Scene): g.E {
 
-  return new g.FilledRect({
+  const background = new g.FilledRect({
     scene: scene,
     cssColor: "#FFFFFF",
     width: g.game.width,
     height: g.game.height,
   });
+
+  // 縦の線
+  const columnLine = new g.FilledRect({
+    scene: scene,
+    cssColor: "#000000",
+    width: 1,
+    height: g.game.height,
+    x: g.game.width / 2,
+    y: 0
+  });
+
+  background.append(columnLine);
+
+  return background;
 }
 
 function createPlayer(scene:g.Scene): g.E {
